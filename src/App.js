bc9ee13db715644d98cef7cb6da78159a6ae89f9
SumaiaSorna <sorna.sumaia@gmail.com>
2022-02-10 21:34:46 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [timer, setTimer] = useState(0);
+
+  const [side, setSide] = useState(50);
+
+  const [color, setColor] = useState("white");
+
+  useEffect(() => {
+    console.log("useEffect", side);
+    if (side % 100 === 0) {
+      setColor("red");
+    } else {
+      setColor("white");
+    }
+  }, [side]);
+
+  const increaseTimer = () => {
+    setTimer(timer + 1);
+    setSide(side + 10);
+  };
+  const decreaseTimer = () => {
+    setTimer(timer - 1);
+    setSide(side - 10);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Timer</h1>
+      <p>{timer}</p>
+      <div>
+        <button onClick={increaseTimer}>Increase</button>
+        <button onClick={decreaseTimer}>Decrease</button>
+      </div>
+      <div
+        style={{
+          border: "1px solid black",
+          width: `${side}px`,
+          height: `${side}px`,
+          backgroundColor: color,
+        }}
+      ></div>
     </div>
   );
-}
+};
 
 export default App;
